edae059680a9a034e25ad9c10b1654c9b7ab9124
sanved77 <sanved77@gmail.com>
2019-11-03 13:13:55 -0500

    Interface enhanced

--- Simulation.java
+++ Simulation.java
@@ -7,26 +7,35 @@ import java.util.Random;
 import java.util.Scanner;
 
 class Simulation extends TimerTask {
-
+    
+    // Prefabs
     public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_BLACK = "\u001B[30m";
     public static final String ANSI_RED = "\u001B[31m";
     public static final String ANSI_GREEN = "\u001B[32m";
-    public static final String ANSI_YELLOW = "\u001B[33m";
-    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_BLUE = "\u001B[35m";
+    public static final String ANSI_ORANGE = "\u001B[34m";
     public static final String ANSI_PURPLE = "\u001B[35m";
     public static final String ANSI_CYAN = "\u001B[36m";
-    public static final String ANSI_WHITE = "\u001B[37m";
+
+    // Constants
     public static final String FIRE = ANSI_RED + "F" + ANSI_RESET;
-    public static final String GRASS = ANSI_GREEN + "G" + ANSI_RESET;
-    public static final int FIRE_SPREAD = 50;
-    public static final int SPEED = 250;
+    public static final String ACT_SENSOR = ANSI_GREEN + "S" + ANSI_RESET;
+    public static final String WARNED_SENSOR = ANSI_GREEN + "S" + ANSI_RESET;
+    public static final String GRASS = " ";
+    public static final String SENSOR = "S";
+
+    // User Constants
+    public static final int FIRE_SPREAD = 75;
+    public static final int SPEED = 700;
+    public static final int SENSORDENSITY = 4;
 
 
-    int nagdiNum = 0;
+    int fireSpread = 0;
+    static int sensorsTriggered = 0;
     static boolean lock = true;
     static Simulation dummy;
     static String map[][] = new String[20][20];
+    static int sensormap[][] = new int[20][20];
     static ArrayList<Posi> list = new ArrayList<>();
 
     static Random rand = new Random();
@@ -57,13 +66,16 @@ class Simulation extends TimerTask {
     public void run() {
         clearScreen();
         spreadFire();
+        // predictFire();
         for(int i = 0; i < 20; i++){
             for(int j = 0; j < 20; j++){
                 System.out.print(" " + map[i][j]);
             }System.out.println("");
         }
-        nagdiNum++;
-        if(nagdiNum > FIRE_SPREAD){
+        System.out.println("\nBlocks affected by fire - " + ANSI_GREEN + (fireSpread + 1) + ANSI_RESET);
+        System.out.println("Sensors Triggered - " + ANSI_GREEN + sensorsTriggered + ANSI_RESET);
+        fireSpread++;
+        if(fireSpread > FIRE_SPREAD){
             synchronized(dummy){
                 dummy.notify();
             }
@@ -116,6 +128,20 @@ class Simulation extends TimerTask {
             }
             else if(map[i][j].contains(GRASS)){
                 map[i][j] = FIRE;
+                for(int k = -1; k <= 1; k++){
+                    for(int l = -1; l <= 1; l++){
+                        if(i < 1 || i > 18 || j < 1 || j > 18){
+                            continue;
+                        }
+                        if(map[i+k][j+l].contains(SENSOR)){
+                            map[i+k][j+l] = ACT_SENSOR;
+                            if(sensormap[i+k][j+l] == 0){
+                                sensormap[i+k][j+l] = 1;
+                                sensorsTriggered++;
+                            }
+                        }
+                    }
+                }
                 list.add(new Posi(i,j));
                 break;
             }
@@ -135,7 +161,11 @@ class Simulation extends TimerTask {
     public static void resetMap(){
         for(int i = 0; i < 20; i++){
             for(int j = 0; j < 20; j++){
-                map[i][j] = GRASS;
+                if(i % SENSORDENSITY == 0 && j % SENSORDENSITY == 0){
+                    map[i][j] = SENSOR;
+                }else{
+                    map[i][j] = GRASS;
+                }
             }
         }
     } 
