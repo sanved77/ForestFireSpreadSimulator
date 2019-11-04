52caf83b9caa5bc704dd3a4603857df090ceb63d
Sanved Tapkeer <sanved77@gmail.com>
2019-11-04 01:14:14 -0500

    Modular map added

--- Simulation.java
+++ Simulation.java
@@ -8,6 +8,14 @@ import java.util.Scanner;
 
 class Simulation extends TimerTask {
     
+    // User Constants
+    public static final int FIRE_SPREAD = 75;
+    public static final int SPEED = 300;
+    public static final int SENSORDENSITY = 3;
+    public static final int L_PADDING_OFFSET = 84;
+    public static final int T_PADDING = 15;
+    public static final int ALGORITHM = 1;
+    
     // Prefabs
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_RED = "\u001B[31m";
@@ -15,10 +23,10 @@ class Simulation extends TimerTask {
     public static final String ANSI_GREEN_BG = "\u001B[42m";
     public static final String ANSI_BLUE = "\u001B[35m";
     public static final String ANSI_YELLOW = "\u001B[33m";
-    public static final String ANSI_ORANGE = "\033[48:2:255:165:0m%s\033[m\n";
     public static final String ANSI_PURPLE = "\u001B[35m";
     public static final String ANSI_CYAN = "\u001B[36m";
     public static final String CLEAR = "\033[H\033[2J";
+    public static final int GRID = 28;
 
     // Constants
     public static final String FIRE = ANSI_YELLOW + "F" + ANSI_RESET;
@@ -26,24 +34,18 @@ class Simulation extends TimerTask {
     public static final String WARNED_SENSOR = ANSI_GREEN + "S" + ANSI_RESET;
     public static final String GRASS = " ";
     public static final String SENSOR = "S";
-
-
-    // User Constants
-    public static final int FIRE_SPREAD = 75;
-    public static final int SPEED = 700;
-    public static final int SENSORDENSITY = 4;
-    public static final int L_PADDING_OFFSET = 84;
-    public static final int T_PADDING = 15;
     public static final String L_PADDING = String.format("%"+ L_PADDING_OFFSET +"s", " ");
 
-
-    int fireSpread = 0;
+    // Variables
+    static int fireSpread = 0;
+    static int sensorsWarned = 0;
     static int sensorsTriggered = 0;
     static boolean lock = true;
     static Simulation dummy;
-    static String map[][] = new String[20][20];
-    static int sensormap[][] = new int[20][20];
+    static String map[][] = new String[GRID][GRID];
+    static int sensormap[][] = new int[GRID][GRID];
     static ArrayList<Posi> list = new ArrayList<>();
+    static ArrayList<Posi> sensorList = new ArrayList<>();
 
     static Random rand = new Random();
     static Scanner scanner = new Scanner(System.in);
@@ -73,21 +75,51 @@ class Simulation extends TimerTask {
     public void run() {
         clearScreen();
         spreadFire();
-        // predictFire();
-        for(int i = 0; i < 20; i++){
-            System.out.print(L_PADDING);
-            for(int j = 0; j < 20; j++){
+        String upperBorder = "";
+        int dGRID = GRID*2+1;
+        for(int i = 0; i <= dGRID; i++){
+            upperBorder+="_";
+        }
+        System.out.print(L_PADDING + upperBorder + "\n");
+        for(int i = 0; i < GRID; i++){
+            System.out.print(L_PADDING + "|");
+            for(int j = 0; j < GRID; j++){
                 System.out.print(" " + map[i][j]);
+                if(j == (GRID-1)){
+                    System.out.print("|");
+                }
             }
             System.out.println("");
         }
+        predictFire();
         System.out.println("\n" + L_PADDING + "Blocks affected by fire - " + ANSI_GREEN + (fireSpread + 1) + ANSI_RESET);
         System.out.println(L_PADDING + "Sensors Triggered - " + ANSI_GREEN + sensorsTriggered + ANSI_RESET);
+        System.out.println(L_PADDING + "Sensors Warned - " + ANSI_GREEN + sensorsWarned + ANSI_RESET);
         fireSpread++;
         if(fireSpread > FIRE_SPREAD){
             synchronized(dummy){
                 dummy.notify();
             }
+        }
+    }
+
+    public static void predictFire(){
+        switch(ALGORITHM){
+            case 1:
+                for(Posi s : sensorList){
+                    for(int k = -SENSORDENSITY; k <= SENSORDENSITY; k+=SENSORDENSITY){
+                        for(int l = -SENSORDENSITY; l <= SENSORDENSITY; l+=SENSORDENSITY){
+                            if(s.getI() < 4 || s.getI() > (GRID-4) || s.getJ() < 4 || s.getJ() > (GRID-4)){
+                                continue;
+                            }
+                            if(map[s.getI()+k][s.getJ()+l].equals(SENSOR)){
+                                map[s.getI()+k][s.getJ()+l] = WARNED_SENSOR;
+                                sensorsWarned++;
+                            }
+                        }
+                    }
+                }
+                break;
         }
     }
 
@@ -132,20 +164,21 @@ class Simulation extends TimerTask {
                     break;
             }
 
-            if(i < 0 || i > 19 || j <0 || j > 19){
+            if(i < 0 || i > (GRID-1) || j < 0 || j > (GRID-1)){
                 continue;
             }
             else if(map[i][j].contains(GRASS)){
                 map[i][j] = FIRE;
                 for(int k = -1; k <= 1; k++){
                     for(int l = -1; l <= 1; l++){
-                        if(i < 1 || i > 18 || j < 1 || j > 18){
+                        if(i < 1 || i > (GRID-1) || j < 1 || j > (GRID-1)){
                             continue;
                         }
-                        if(map[i+k][j+l].contains(SENSOR)){
+                        if(map[i+k][j+l].equals(SENSOR) || map[i+k][j+l].equals(WARNED_SENSOR)){
                             map[i+k][j+l] = ACT_SENSOR;
                             if(sensormap[i+k][j+l] == 0){
                                 sensormap[i+k][j+l] = 1;
+                                sensorList.add(new Posi(i+k, j+l));
                                 sensorsTriggered++;
                             }
                         }
@@ -178,9 +211,9 @@ class Simulation extends TimerTask {
     }
 
     public static void resetMap(){
-        for(int i = 0; i < 20; i++){
-            for(int j = 0; j < 20; j++){
-                if(i % SENSORDENSITY == 0 && j % SENSORDENSITY == 0){
+        for(int i = 0; i < GRID; i++){
+            for(int j = 0; j < GRID; j++){
+                if(i != 0 && j != 0 && i % SENSORDENSITY == 0 && j % SENSORDENSITY == 0){
                     map[i][j] = SENSOR;
                 }else{
                     map[i][j] = GRASS;
